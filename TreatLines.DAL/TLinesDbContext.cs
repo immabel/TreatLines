7c537a537d5e7a7ddc4b7ad8327b10a48e094a13
immabel <maryna.tymchenko@nure.ua>
2021-07-20 12:33:30 +0300

    Added constants for rolenames, dbcontext

---
+++ TreatLines.DAL/TLinesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TreatLines.DAL.Entities;
+
+namespace TreatLines.DAL
+{
+    public class TLinesDbContext : IdentityDbContext<User>
+    {
+        public TLinesDbContext(DbContextOptions<TLinesDbContext> options)
+            : base(options)
+        { }
+        public DbSet<HospitalAdmin> HospitalAdmins { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<DoctorPatient> DoctorPatients { get; set; }
+        public DbSet<RequestToCreateHospital> RequestsToCreateHospital { get; set; }
+        public DbSet<RequestToCreatePatient> RequestsToCreatePatient { get; set; }
+    }
+}
